8ac70fd53eddc08e7d422db27dd8b606ccebd768
Remo Mueller <remosm@gmail.com>
2015-06-30 11:20:54 -0400

    Version bump to 0.3.0.pre

--- lib/nsrr/version.rb
+++ lib/nsrr/version.rb
@@ -1,9 +1,9 @@
 module Nsrr
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 2
+    MINOR = 3
     TINY = 0
-    BUILD = nil # "pre", "rc", "rc2", nil
+    BUILD = "pre" # "pre", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
