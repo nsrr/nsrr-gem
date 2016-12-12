0ff20bff94b01313a4573b4cf11d597e00ad9091
Remo Mueller <remosm@gmail.com>
2016-12-12 10:48:06 -0500

    Version bump to 0.4.0.beta1

--- lib/nsrr/version.rb
+++ lib/nsrr/version.rb
@@ -5,7 +5,7 @@ module Nsrr
     MAJOR = 0
     MINOR = 4
     TINY = 0
-    BUILD = 'pre' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = 'beta1' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
